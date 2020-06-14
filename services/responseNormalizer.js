337724019d74e4cea60a446aada46abe5d420532
Glenn Mosdall <glenn@getroadmap.com>
2020-06-14 17:34:52 +0200

    Structured ResponseNormalizer with all the necessary data from CovidControls.

--- services/responseNormalizer.js
+++ services/responseNormalizer.js
@@ -2,25 +2,62 @@ class ResponseNormalizer {
 
     getResponse() {
         const response = {};
+
         if (this.geoName) {
-            response.cityName = this.geoName.cityName;
-            response.stateCode = this.geoName.stateCode;
-            response.countryName = this.geoName.countryName;
-            response.countryCode = this.geoName.countryCode;
+            response.geoInfo = {};
+            response.geoInfo.cityName = this.geoName.cityName;
+            response.geoInfo.stateCode = this.geoName.stateCode;
+            response.geoInfo.countryName = this.geoName.countryName;
+            response.geoInfo.countryCode = this.geoName.countryCode;
         }
 
         if (this.cityRating) {
-            response.colleagueRating = this.cityRating.colleagueRating;
-            response.environmentalImpact = this.cityRating.environmentalImpact;
-            response.wellBeing = this.cityRating.wellBeing;
+            response.ratings = {};
+            response.ratings.colleagueRating = this.cityRating.colleagueRating;
+            response.ratings.environmentalImpact = this.cityRating.environmentalImpact;
+            response.ratings.wellBeing = this.cityRating.wellBeing;
         }
 
+        //Starting with separate pages
+        response.pages = {}
+        response.pages.country = {};
+        response.pages.city = {};
+        response.pages.wellBeing = {};
+        response.pages.sustainability = {};
+        response.pages.environment = {};
+        response.pages.trustedPartners = {};
+
         if (this.covidControls) {
-            this.printJson(this.covidControls);
-            response.infected = this.covidControls.infected;
+            response.pages.country.totalCases = this.covidControls.sick;
+            response.pages.country.deathCases = this.covidControls.dead;
+            response.pages.country.lockDown = this.covidControls.lockdownInfo.lockdown;
+            response.pages.country.lastUpdated = this.getDateFromTimestamp(this.covidControls.lastUpdated);
+            response.pages.country.recovered = this.getRecovered(this.covidControls.recovered);
+
+            response.pages.city.publicTransport = this.covidControls.lockdownInfo.transportMoreInfo;
+
+            response.pages.city.restaurantsAndBars = this.covidControls.lockdownInfo.restaurantsAndBars;
+            response.pages.city.shopping = this.covidControls.lockdownInfo.shopping;
+            response.pages.city.eventMoreInfo = this.covidControls.lockdownInfo.eventMoreInfo;
+            response.pages.city.touristAttractions = this.covidControls.lockdownInfo.touristAttractions;
         }
 
         return JSON.stringify(response, null, 2);
+    }
+
+    getDateFromTimestamp(timestamp) {
+        const date = new Date(this.covidControls.lastUpdated);
+        //Just hardcode US date formatting
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+    }
+
+    getRecovered(recovered) {
+        let normalizedRecovered = 'Not reported';
+        if (recovered && recovered > 0) {
+            normalizedRecovered = recovered
+        }
+
+        return normalizedRecovered;
     }
 
     setGeoName(geoName) {
