b510e13d16f4315387d799d89bea1f9b4017ea9e
Glenn Mosdall <glenn@getroadmap.com>
2020-06-14 11:50:54 +0200

    Getting response from covidControls.

--- services/covidControls.js
+++ services/covidControls.js
@@ -1,6 +1,12 @@
+const got = require('got');
 
-function covidControls() {
-    return "covidControls";
+async function covidControls() {
+    const headers = {'Authorization': 'Bearer getroadmap'};
+    const response = await got('https://prod.greatescape.co/api/travel/countries/corona/', {
+        headers: headers
+    });
+
+    return response.body
 }
 
 module.exports = covidControls;
